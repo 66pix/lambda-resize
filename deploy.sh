6e5e3d3f4d03c56366717f6dfdce282e97c866ee
Michael Robinson <mike@pagesofinterest.net>
2016-01-23 22:21:10 +1300

    Set some sizes for staging

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,41 @@
 set -e
 
 ENVIRONMENT=$1
+SIZES=$2
+
+DESTINATION_BUCKET=""
+if ($ENVIRONMENT === "master"); then
+  DESTINATION_BUCKET=$PRODUCTION_BUCKET
+else
+  DESTINATION_BUCKET=$STAGING_BUCKET
+fi
 
 echo ""
 echo "Installing awscli"
-sudo pip install --upgrade docker-py\<1.2 requests\<2.7 awscli
+sudo pip install --upgrade awscli
 
-# Copy _config.json
+echo ""
+echo "Installing node-lambda"
+npm install -g node-lambda
 
-# Replace %%DESTIONATION_BUCKET%%, %%SIZES%% from env vars
+echo ""
+echo "Preparing config.json"
+cp _config.json config.json
+sed -i "s/DESTINATION_BUCKET/$DESTINATION_BUCKET/g" config.json
+sed -i "s/SIZES/$SIZES/g" config.json
+
+echo ""
+echo "Deploying to {$ENVIRONMENT}"
+node-lambda \
+  --environment "$ENVIRONMENT" \
+  --accessKey $AWS_KEY \
+  --secretKey $AWS_SECRET \
+  --region ap-southeast-2 \
+  --functionName resize-on-upload \
+  --handler index.handler \
+  # --role "$AWS_LAMBDA_ARN "\
+  --description "Creates resized copies of images on $DESTINATION_BUCKET when uploads occur"
+
+
 
 # Deploy to lambda
