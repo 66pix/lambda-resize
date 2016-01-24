6f4d431290c4b760e27e14ec5264906310bb3e5b
Michael Robinson <mike@pagesofinterest.net>
2016-01-25 00:37:47 +1300

    Upped memory and timeout

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,8 @@ echo "Deploying to $ENVIRONMENT"
 ./node_modules/node-lambda/bin/node-lambda deploy \
   --description "Resize uploaded images to $SIZES on $DESTINATION_BUCKET" \
   --environment "$ENVIRONMENT" \
-  --timeout 10 \
+  --timeout 20 \
+  --memorySize 192 \
   --accessKey "$AWS_KEY" \
   --secretKey "$AWS_SECRET" \
   --functionName "${ENVIRONMENT}-resize-on-upload" \
