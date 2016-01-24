d84ecaefdc36e9a97bd83d949aac22d939325dcd
Michael Robinson <mike@pagesofinterest.net>
2016-01-25 00:21:50 +1300

    More debugging

--- functions/image-processor.js
+++ functions/image-processor.js
@@ -1,10 +1,13 @@
 'use strict';
 
+/* eslint-disable no-console */
+
 var filename = require('filename.js');
 var Promise = require('bluebird');
 
 module.exports = function imageProcessor(image) {
   return function processImageToWidth(width, suffix) {
+    console.log(width, suffix);
     var params = {
       srcPath: '-',
       srcData: image.data,
@@ -15,15 +18,21 @@ module.exports = function imageProcessor(image) {
     };
 
     var imagemagick = require('imagemagick');
+    console.log('imagemagick');
     return new Promise(function(resolve, reject) {
+      console.log('resize');
       imagemagick.resize(params, function(error, stdout, stderr) {
+        console.log(error);
+        console.log(stderr);
         if (error || stderr) {
           return reject('imagemagick error: ' + (error || stderr));
         }
+        console.log('no errors');
         var suffixes = ['w' + width];
         if (suffix) {
           suffixes.push(suffix);
         }
+        console.log(suffixes);
         resolve({
           key: filename.appendSuffix(suffixes, image.key),
           data: stdout,
